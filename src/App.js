f55624df650b232ed420749026eed2638d1cff92
yaaqin <yaqin.me@gmail.com>
2023-10-31 21:03:28 +0700

    [ADD]login pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter ([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter ([
   {
     path: "/detail",
     element: <Detail/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
   }
 ])
 
